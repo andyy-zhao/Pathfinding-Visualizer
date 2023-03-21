410e959ab362f48f855d4431d738cb9a81fe0fc4
chickenstripths <azhao24@hotmail.com>
2023-03-20 23:44:28 -0400

    first

---
+++ pathfinding_visualizer/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { PathfindingVisualizer } from './Pathfinder/PathfindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer />
+    </div>
+  );
+}
+
+export default App;
